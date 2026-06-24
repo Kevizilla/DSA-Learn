0f34922baa24143b3b458206337a5d973311e5d6
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-24 21:45:16 +0530

    Added insert method to BST

---
+++ binary_search_tree/BinarySearchTree.java
@@ -0,0 +1,22 @@
+package binary_search_tree;
+
+public class BinarySearchTree {
+    private  Node root;
+
+    public void insert(int data) {
+    root = insert(root, data);
+    }
+
+    private Node insert(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+        if (data < node.data) {
+            node.left = insert(node.left, data);
+        }
+        else if (data > node.data) {
+            node.right = insert(node.right, data);
+        }
+        return node;
+    }
+}
