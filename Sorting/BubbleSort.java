49cd9c0be1acdb889c73e100c85895ac3e860548
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-24 19:18:43 +0530

    Fixed bubble sort and selection sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,10 +1,7 @@
 package Sorting;
 
-import java.util.Arrays;
-
 public class BubbleSort {
-    public static void main(String[] args){
-        int[] list = {6, 3, 7, 9, 2, 11, 15, 0};
+    public static int[] sort(int[] list) {
         for (int i = list.length - 1; i > 0; i--) {
             boolean swapped = false;
             for (int j = 0; j < i; j++) {
@@ -15,8 +12,8 @@ public class BubbleSort {
                     swapped = true;
                 }
             }
-            if(!swapped) break;
+            if (!swapped) break;
         }
-        System.out.println(Arrays.toString(list));
+        return list;
     }
 }
