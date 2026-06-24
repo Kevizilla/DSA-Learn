2ac81b137bb4e607d78fa6c4581547ded568e7bb
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-24 19:17:54 +0530

    Fixed bubble sort and selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,27 +1,22 @@
 package Sorting;
 
-import java.util.Arrays;
+public class SelectionSort {
 
-class SelectionSort {
-    public static void main(String[] args){
-        int[] list = {5, 13, 2, 8 ,11 ,9, 14};
-        for(int i = 0; i < list.length; i++){
-            int minVal =  list[i];
+    public static int[] sort(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            int minVal = list[i];
             int minIndex = i;
-            for(int j = i+1; j < list.length; j++){
-                if(list[j] < minVal){
+            for (int j = i + 1; j < list.length; j++) {
+                if (list[j] < minVal) {
                     minVal = list[j];
                     minIndex = j;
                 }
             }
-            if(minVal == list[i]){
-                continue;
-            }
-            else {
+            if (minIndex != i) {
                 list[minIndex] = list[i];
                 list[i] = minVal;
             }
         }
-        System.out.println(Arrays.toString(list));
+        return list;
     }
 }
