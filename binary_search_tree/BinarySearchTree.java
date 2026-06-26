fb1d1a94154c0fc6c807f610017748dcc51add8b
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-26 11:59:36 +0530

    Added search method to BST

--- binary_search_tree/BinarySearchTree.java
+++ binary_search_tree/BinarySearchTree.java
@@ -1,22 +1,40 @@
 package binary_search_tree;
 
 public class BinarySearchTree {
-    private  Node root;
+    private Node root;
 
     public void insert(int data) {
     root = insert(root, data);
     }
 
-    private Node insert(Node node, int data) {
-        if (node == null) {
+    private Node insert(Node node, int data){
+        if(node == null){
             return new Node(data);
         }
-        if (data < node.data) {
+        if(data < node.data){
             node.left = insert(node.left, data);
         }
-        else if (data > node.data) {
+        else if(data > node.data){
             node.right = insert(node.right, data);
         }
         return node;
     }
+
+    public boolean search(int data) {
+        return search(root, data);
+    }
+
+    private boolean search(Node node, int data){
+        if (node == null){
+            return false;
+        }
+        else  if (data == node.data){
+            return true;
+        }
+        else if (data < node.data){
+            return search(node.left, data);
+        }
+        return  search(node.right, data);
+    }
+
 }
