6c3c64a113d9fee8cf9e7a0640a9b0f3af443bcf
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-27 19:39:10 +0530

    Added the method Height

--- binary_search_tree/BinarySearchTree.java
+++ binary_search_tree/BinarySearchTree.java
@@ -97,5 +97,16 @@ public class BinarySearchTree {
         return max;
     }
 
+    public int height() {
+        return height(root);
+    }
 
+    private int height(Node node) {
+        if(node == null){
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
