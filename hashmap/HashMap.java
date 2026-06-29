a1f6de795464bfed4e806caa6d7d51bd6dcca891
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-29 18:53:20 +0530

    I added a put method.

---
+++ hashmap/HashMap.java
@@ -0,0 +1,39 @@
+package hashmap;
+
+import java.util.LinkedList;
+
+public class HashMap {
+    private LinkedList<Entry>[] buckets;  // array of linked lists
+    private int capacity;
+
+    HashMap(int capacity) {
+        this.capacity = capacity;
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(String key) {
+        return Math.abs(key.hashCode() % capacity);
+    }
+
+    public void put(String key, int value) {
+        int index = hash(key);
+        for(Entry e : buckets[index]) {
+            if(e.key.equals(key)){
+                e.value = value;
+                return;
+            }
+        }
+        buckets[index].add(new Entry(key, value));
+    }
+
+    public int get(String key) {
+        // TODO — return -1 if not found
+    }
+
+    public void remove(String key) {
+        // TODO
+    }
+}
