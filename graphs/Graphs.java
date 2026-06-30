72cfbf99b0f6c9ddcce996653245e626c998518f
Kevin Stephen <b.kevinstephen@gmail.com>
2026-06-30 23:11:57 +0530

    Created graph class with three methods

---
+++ graphs/Graphs.java
@@ -0,0 +1,31 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graphs {
+    private Map<Integer, List<Integer>> adjList;
+
+    public Graphs() {
+        adjList = new HashMap<>();
+    }
+
+    public void addVertex(int vertex) {
+        adjList.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    public void addEdge(int v1, int v2) {
+        addVertex(v1);
+        addVertex(v2);
+        adjList.get(v1).add(v2);
+        adjList.get(v2).add(v1);
+    }
+
+    public void display() {
+        for(int i : adjList.keySet()) {
+            System.out.println(i + " -> " + adjList.get(i));
+        }
+    }
+}
