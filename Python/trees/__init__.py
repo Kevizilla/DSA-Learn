b5dfb47fab4e428a26c46392e765e9dd4f667adf
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-11 21:18:29 +0530

    Skeleton

---
+++ Python/trees/__init__.py
@@ -0,0 +1,2 @@
+from .binary_trees import BinaryTree
+from.binary_search_tree import BST
---
+++ Python/trees/binary_search_tree.py
@@ -0,0 +1,26 @@
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        # TODO
+        pass
+
+    def search(self, data):
+        # TODO — return True/False
+        pass
+
+    def delete(self, data):
+        # TODO — the hard one, save for last
+        pass
+
+    def inorder(self):
+        # same as before — but now the output will come out SORTED
+        pass
