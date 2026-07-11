73229dbce912e0615bb399756243dc91c92548ae
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-11 16:40:03 +0530

    added a insert and a display function

--- Python/linked_list/linked_list.py
+++ Python/linked_list/linked_list.py
@@ -32,15 +32,29 @@ class LinkedList:
         self.length += 1
 
     def insert(self, index, data):
-        if index == self.length:
-            self.append(data)
-            return
-        elif index == 0:
-            self.prepend(data)
-        elif index > self.length:
-            raise IndexError
-        else:
-            pass
+        try:
+            if index == self.length:
+                self.append(data)
+                return
+            elif index == 0:
+                self.prepend(data)
+            elif index > self.length or index < 0:
+                raise IndexError("Index out of range")
+            else:
+                new_node = Node(data)
+                current_node = self.head
+                i = 1
+                while current_node.next is not None:
+                    if index == i:
+                        new_node.next = current_node.next
+                        current_node.next = new_node
+                        self.length += 1
+                        return
+                    i += 1
+                    current_node = current_node.next
+
+        except IndexError:
+            print("Index out of range")
 
 
     def delete(self, data):
@@ -50,6 +64,14 @@ class LinkedList:
     def find(self, data):
         # TODO: return index of first match, or -1
         pass
+
+    def display(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
 
     def __repr__(self):
         # TODO: return something like "1 -> 2 -> 3 -> None"
