56b83047405277fea7c6cf9d5c7073e85b08c161
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-14 10:22:45 +0530

    Created graph package

---
+++ Python/graphs/graphs.py
@@ -0,0 +1,18 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_vertex(self, vertex):
+        pass
+
+    def add_edge(self, u, v):
+        pass
+
+    def remove_edge(self, u, v):
+        pass
+
+    def remove_vertex(self, vertex):
+        pass
+
+    def __repr__(self):
+        pass
