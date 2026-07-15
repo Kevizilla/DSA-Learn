7a098ee900ad1701be3fee7f3e40b50fb2de38cd
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-15 22:11:03 +0530

    Added max_sum_subarray func

--- Python/algorithms/sliding_window/fixed_window.py
+++ Python/algorithms/sliding_window/fixed_window.py
@@ -1,1 +1,20 @@
-def max_sum_subarray(arr, k):
+def max_sum_subarray(lst, k):
+    if not lst:
+        return "Enter valid list"
+
+    if k <= 0:
+        return "k has to be greater than 0"
+
+    if k > len(lst):
+        return "k cannot be greater than len(lst)"
+
+    # Compute the first window's sum
+    current_sum = sum(lst[:k])
+    max_sum = current_sum
+    # Slide the window one step at a time
+    for i in range(0, len(lst)-k):
+        current_sum = current_sum - lst[i] + lst[i+k]
+        if current_sum > max_sum:
+            max_sum = current_sum
+
+    return max_sum
