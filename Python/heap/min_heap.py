f74eb778e788fc09446407270c103e91442c1ecb
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-15 18:17:08 +0530

    I added a remove min function

--- Python/heap/min_heap.py
+++ Python/heap/min_heap.py
@@ -11,6 +11,9 @@ class MinHeap:
     def _right(self, index):
         return 2 * index + 2
 
+    def _swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
     def insert(self, value):
         self.heap.append(value)
         index = len(self.heap) - 1
@@ -19,7 +22,46 @@ class MinHeap:
             parent_index = self._parent(index)
 
             if self.heap[parent_index] > self.heap[index]:
-                self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+                self._swap(index, parent_index)
                 index = parent_index
             else:
                 break
+
+    def remove_min(self):
+        # Handle empty heap
+        if not self.heap:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        min_value = self.heap[0]
+
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        current_index = 0
+        # Bubble down
+        while True:
+            left_index = self._left(current_index)
+            right_index = self._right(current_index)
+            if left_index < len(self.heap):
+                if right_index < len(self.heap):
+                    if self.heap[current_index] > min(self.heap[left_index], self.heap[right_index]):
+                        if self.heap[current_index] > self.heap[left_index]:
+                            self._swap(current_index, left_index)
+                            current_index = left_index
+                        else:
+                            self._swap(current_index, right_index)
+                            current_index = right_index
+                    else:
+                        break
+                else:
+                    if self.heap[current_index] > self.heap[left_index]:
+                        self._swap(current_index, left_index)
+                        current_index = left_index
+                    else:
+                        break
+            else:
+                break
+
+        return min_value
