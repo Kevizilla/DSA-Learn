7df36b4bac41dff6e053e6f11e3c4d971e1c73f2
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-15 22:26:22 +0530

    Added average_of_subarray func

--- Python/algorithms/sliding_window/fixed_window.py
+++ Python/algorithms/sliding_window/fixed_window.py
@@ -8,13 +8,30 @@ def max_sum_subarray(lst, k):
     if k > len(lst):
         return "k cannot be greater than len(lst)"
 
-    # Compute the first window's sum
     current_sum = sum(lst[:k])
     max_sum = current_sum
-    # Slide the window one step at a time
-    for i in range(0, len(lst)-k):
-        current_sum = current_sum - lst[i] + lst[i+k]
+    for i in range(k, len(lst)):
+        current_sum = current_sum + lst[i] - lst[i - k]
         if current_sum > max_sum:
             max_sum = current_sum
 
     return max_sum
+
+def average_of_subarray(lst, k):
+    if not lst:
+        return "Enter valid list"
+
+    if k <= 0:
+        return "k has to be greater than 0"
+
+    if k > len(lst):
+        return "k cannot be greater than len(lst)"
+
+    current_sum = sum(lst[:k])
+    result = [current_sum/k]
+
+    for i in range(k, len(lst)):
+        current_sum = current_sum + lst[i] - lst[i - k]
+        result.append(current_sum/k)
+
+    return result
