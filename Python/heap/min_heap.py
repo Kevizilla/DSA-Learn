67b5c960434e58d7a6873bd258eea761086ecb35
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-15 19:38:55 +0530

    added func heap_sort

--- Python/heap/min_heap.py
+++ Python/heap/min_heap.py
@@ -1,9 +1,9 @@
 class MinHeap:
     def __init__(self, values=None):
         if values is None:
-            self.heap = []
+            self._heap = []
         else:
-            self.heap = values.copy()
+            self._heap = values.copy()
             self.heapify()
 
     def _parent(self, index):
@@ -16,58 +16,68 @@ class MinHeap:
         return 2 * index + 2
 
     def _swap(self, i, j):
-        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+        self._heap[i], self._heap[j] = self._heap[j], self._heap[i]
 
     def _bubble_up(self, index):
         while index > 0:
             parent = self._parent(index)
 
-            if self.heap[parent] <= self.heap[index]:
+            if self._heap[parent] <= self._heap[index]:
                 break
 
             self._swap(parent, index)
             index = parent
 
     def insert(self, value):
-        self.heap.append(value)
-        self._bubble_up(len(self.heap) - 1)
+        self._heap.append(value)
+        self._bubble_up(len(self._heap) - 1)
 
     def _bubble_down(self, index):
         while True:
             left = self._left(index)
             right = self._right(index)
 
-            if left >= len(self.heap):
+            if left >= len(self._heap):
                 break
 
             smallest = left
 
-            if right < len(self.heap) and self.heap[right] < self.heap[left]:
+            if right < len(self._heap) and self._heap[right] < self._heap[left]:
                 smallest = right
 
-            if self.heap[index] <= self.heap[smallest]:
+            if self._heap[index] <= self._heap[smallest]:
                 break
 
             self._swap(index, smallest)
             index = smallest
 
     def remove_min(self):
-        if not self.heap:
+        if not self._heap:
             return None
 
-        if len(self.heap) == 1:
-            return self.heap.pop()
+        if len(self._heap) == 1:
+            return self._heap.pop()
 
-        min_value = self.heap[0]
-        self.heap[0] = self.heap.pop()
+        min_value = self._heap[0]
+        self._heap[0] = self._heap.pop()
 
         self._bubble_down(0)
 
         return min_value
 
     def heapify(self):
-        current = len(self.heap) // 2 - 1
+        current = len(self._heap) // 2 - 1
 
         while current >= 0:
             self._bubble_down(current)
             current -= 1
+
+    def heap_sort(arr):
+        heap = MinHeap(arr)
+
+        result = []
+
+        while heap._heap:
+            result.append(heap.remove_min())
+
+        return result
