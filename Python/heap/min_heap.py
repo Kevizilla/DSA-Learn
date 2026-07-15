12ddc80aa9796c0d4c3c8bf04acf7aa7b34c0f4c
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-15 17:07:36 +0530

    I added an insert function

--- Python/heap/min_heap.py
+++ Python/heap/min_heap.py
@@ -2,5 +2,24 @@ class MinHeap:
     def __init__(self):
         self.heap = []
 
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _left(self, index):
+        return 2 * index + 1
+
+    def _right(self, index):
+        return 2 * index + 2
+
     def insert(self, value):
-        pass
+        self.heap.append(value)
+        index = len(self.heap) - 1
+
+        while index > 0:
+            parent_index = self._parent(index)
+
+            if self.heap[parent_index] > self.heap[index]:
+                self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+                index = parent_index
+            else:
+                break
