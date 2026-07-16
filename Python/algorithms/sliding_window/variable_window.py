d05681df5f12e34a3099dedd8c42da0ce5452b28
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-16 11:28:32 +0530

    added longest_unique_subarray func

--- Python/algorithms/sliding_window/variable_window.py
+++ Python/algorithms/sliding_window/variable_window.py
@@ -21,3 +21,23 @@ def smallest_subarray(lst, s):
         return 0
 
     return min_len
+
+# To find the length of the longest substring with no repeated characters.
+def longest_unique_substring(string):
+    start = 0
+    window = set()
+    max_len = 0
+
+    for end in range(len(string)):
+        char = string[end]
+
+        while char in window:
+            window.remove(string[start])
+            start += 1
+
+        window.add(char)
+        current_len = (end - start + 1)
+        if current_len > max_len:
+            max_len = current_len
+
+    return max_len
