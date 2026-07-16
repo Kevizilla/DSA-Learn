cfe97d5a383e890b0fc13d2d8bd9041f1adb457b
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-16 10:48:33 +0530

    added smallest_subarray func

--- Python/algorithms/sliding_window/variable_window.py
+++ Python/algorithms/sliding_window/variable_window.py
@@ -1,2 +1,23 @@
-def smallest_subarray(arr, S):
-    pass
+from math import inf
+
+#To find the length of the smallest contiguous subarray whose sum is greater than or equal to S
+def smallest_subarray(lst, s):
+    start = 0
+    min_len = inf
+    window_sum = 0
+
+    for end in range(len(lst)):
+        window_sum += lst[end]
+
+        while window_sum >= s:
+            current_len = (end - start + 1)
+            if current_len < min_len:
+                min_len = current_len
+
+            window_sum -= lst[start]
+            start += 1
+
+    if min_len == inf:
+        return 0
+
+    return min_len
