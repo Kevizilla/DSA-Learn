c4d7b1a9f0b8d5f0518b97258d5e6ab012546596
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-16 21:12:22 +0530

    skeleton

---
+++ Python/algorithms/two_pointer/two_pointer.py
@@ -0,0 +1,18 @@
+def two_sum_sorted(lst, target):
+    left = 0
+    right = len(lst) - 1
+
+    while left < right:
+
+        current_sum = ...
+
+        if current_sum == target:
+            ...
+
+        elif current_sum < target:
+            ...
+
+        else:
+            ...
+
+    return None
