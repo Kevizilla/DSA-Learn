2dde3f9c6b869d22df3fb9e2600122de2a3b9533
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-16 11:51:55 +0530

    optimization using dict instead of set

--- Python/algorithms/sliding_window/__init__.py
+++ Python/algorithms/sliding_window/__init__.py
@@ -1,4 +1,4 @@
 from .fixed_window import max_sum_subarray, average_of_subarray
+from .variable_window import smallest_subarray, longest_unique_substring
 
-
-__all__ = ["max_sum_subarray", "average_of_subarray"]
+__all__ = ["max_sum_subarray", "average_of_subarray", "smallest_subarray", "longest_unique_substring"]
