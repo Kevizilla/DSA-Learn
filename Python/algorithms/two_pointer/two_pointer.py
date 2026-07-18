5bfc683ea70a65155144ca53340471cc4271b326
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-18 13:16:25 +0530

    Added sorted sqrt function

--- Python/algorithms/two_pointer/two_pointer.py
+++ Python/algorithms/two_pointer/two_pointer.py
@@ -26,3 +26,22 @@ def remove_duplicates(lst):
             lst[write] = lst[read]
 
     return write + 1
+
+def sorted_squares(lst):
+    left = 0
+    right = len(lst) - 1
+    write = len(lst) - 1
+
+    result = [0] * len(lst)
+
+    while left <= right:
+        if abs(lst[left]) > abs(lst[right]):
+            result[write] = lst[left] ** 2
+            left += 1
+        else:
+            result[write] = lst[right] ** 2
+            right -= 1
+
+        write -= 1
+
+    return result
