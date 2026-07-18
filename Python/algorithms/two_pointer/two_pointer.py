498e9bc46e11028f743ad7c58e278beb6e2b8cad
Kevin Stephen <b.kevinstephen@gmail.com>
2026-07-18 13:22:45 +0530

    Added is_palindrome function

--- Python/algorithms/two_pointer/two_pointer.py
+++ Python/algorithms/two_pointer/two_pointer.py
@@ -50,8 +50,16 @@ def is_palindrome(string):
     left, right = 0, len(string) - 1
 
     while left < right:
+        if not string[left].isalnum():
+            left += 1
+            continue
+        if not string[right].isalnum():
+            right -= 1
+            continue
+
         if string[left].lower() != string[right].lower():
             return False
+
         left += 1
         right -= 1
 
